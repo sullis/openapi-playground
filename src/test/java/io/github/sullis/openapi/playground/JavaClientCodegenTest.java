65c9f8a1f4ef82732379679a2d4a2e29370fd621
sullis <github@seansullivan.com>
2024-05-05 18:24:48 -0700

    use junit ParameterizedTest

--- src/test/java/io/github/sullis/openapi/playground/JavaClientCodegenTest.java
+++ src/test/java/io/github/sullis/openapi/playground/JavaClientCodegenTest.java
@@ -5,10 +5,16 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.core.models.ParseOptions;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openapitools.codegen.ClientOptInput;
 import org.openapitools.codegen.DefaultGenerator;
 import org.openapitools.codegen.languages.JavaClientCodegen;
@@ -17,16 +23,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class JavaClientCodegenTest {
+  private static final List<String> CLIENT_LIBRARY_NAMES = List.of(
+      JavaClientCodegen.NATIVE
+  );
 
-  @Test
-  void generateCloudflareClient(TestInfo testInfo) {
-    generateJavaClient(testInfo, "cloudflare", JavaClientCodegen.APACHE, "https://raw.githubusercontent.com/cloudflare/api-schemas/main/openapi.json");
+  private static final Map<String, String> OPENAPI_SPECS = Map.of(
+    "cloudflare", "https://raw.githubusercontent.com/cloudflare/api-schemas/main/openapi.json"
+  );
+
+  static private Stream<Arguments> targets() {
+    List<Arguments> argumentsList = new ArrayList<>();
+    for (String openapiSpecName : OPENAPI_SPECS.keySet()) {
+      final String openapiSpecUrl = OPENAPI_SPECS.get(openapiSpecName);
+      for (String clientLibrary : CLIENT_LIBRARY_NAMES) {
+        argumentsList.add(Arguments.of(clientLibrary, openapiSpecName, openapiSpecUrl));
+      }
+    }
+    return argumentsList.stream();
   }
 
-  private void generateJavaClient(final TestInfo testInfo, final String name, final String codegenLibrary, final String url) {
-    final String testClass = testInfo.getTestClass().get().getSimpleName();
-    final String testMethod = testInfo.getTestMethod().get().getName();
-    final File outputDir = new File("./target/" + testClass + "-" + testMethod + "-" + System.currentTimeMillis());
+  @ParameterizedTest
+  @MethodSource("targets")
+  void generateJavaClient(final String codegenLibrary, final String openapiSpecName, final String url) {
+    final String testClass = this.getClass().getSimpleName();
+    final File outputDir = new File("./target/" + testClass + "-" + openapiSpecName + "-" + codegenLibrary + "-" + System.currentTimeMillis());
     outputDir.mkdirs();
     JavaClientCodegen codegen = new JavaClientCodegen();
     codegen.setDoNotUseRx(true);
@@ -43,7 +63,7 @@ public class JavaClientCodegenTest {
 
     DefaultGenerator generator = new DefaultGenerator();
 
-    final String basePackage = "codegen." + name.toLowerCase() + ".client";
+    final String basePackage = "codegen." + openapiSpecName.toLowerCase() + ".client";
     codegen.setModelPackage(basePackage + ".model");
     codegen.setApiPackage(basePackage + ".api");
     codegen.setInvokerPackage(basePackage);
@@ -53,7 +73,9 @@ public class JavaClientCodegenTest {
 
     List<File> generatedFiles = generator.opts(generatorInput).generate();
     for (File f : generatedFiles) {
-      assertThat(f).isNotEmpty();
+      if (f.isFile() && f.getName().endsWith(".java")) {
+        assertThat(f).isNotEmpty();
+      }
     }
   }
 }
